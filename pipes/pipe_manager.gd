42933e97c2043ea5cd869ad98172d6bd35922986
yukkie8058 <yukkie8058@gmail.com>
2025-06-30 00:40:41 +0900

    PipeManagerのキャッシュ機能を一旦削除し、Idle状態に入った時にパイプ達を消すように。 Fix #3
    キャッシュ機能はパフォーマンス的に効果があるのか微妙だし、スペルミスが恥ずかし過ぎるので一旦削除する。
    
    あとついでにコードのフォーマットも調整。

--- pipes/pipe_manager.gd
+++ pipes/pipe_manager.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 var pipes: Array[Pipe] = []
 signal pipe_appended()
-var cash_pipes: Array[Pipe] = []
 
 @export var _interval: float = 250
 @export var _gap_length: float = 150
@@ -15,10 +14,11 @@ var _next_texture: int = -1
 func _init() -> void:
 	_next_texture = randf() * _texture_pairs.size() as int
 
+func _idle_state_entered() -> void:
+	_pipes_clear()
+
 func _game_state_entered() -> void:
-	for i in pipes: i.queue_free()
-	pipes.clear()
-	_index = 0
+	_pipes_clear()
 
 func _game_state_processing(_delta: float) -> void:
 	var camera_rect := get_viewport_rect() * get_canvas_transform()
@@ -28,14 +28,14 @@ func _game_state_processing(_delta: float) -> void:
 		(_texture_pairs.values()[_next_texture] as Texture2D).get_width()
 	)
 	if pipes.is_empty() or pipes[-1].global_position.x < camera_rect.end.x + width - _interval:
-		if cash_pipes.is_empty():
-			pipes.append(Pipe.new())
-		else:
-			pipes.append(cash_pipes.pop_front())
+		pipes.append(Pipe.new())
 		pipes[-1].index = _index
 		_index += 1
 		pipes[-1].global_position = Vector2(camera_rect.end.x + width / 2, 0)
-		pipes[-1].upper_y = randf_range(-get_viewport_rect().size.y / 2, get_viewport_rect().size.y / 2 - _gap_length)
+		pipes[-1].upper_y = randf_range(
+			-get_viewport_rect().size.y / 2,
+			get_viewport_rect().size.y / 2 - _gap_length,
+		)
 		pipes[-1].lower_y = pipes[-1].upper_y + _gap_length
 		pipes[-1].texture_top = _texture_pairs.keys()[_next_texture]
 		pipes[-1].texture_middle = _texture_pairs.values()[_next_texture]
@@ -43,10 +43,11 @@ func _game_state_processing(_delta: float) -> void:
 		pipe_appended.emit()
 	
 	if pipes.size() > 1 and not pipes[0].get_global_rect().intersects(camera_rect):
-		assert(cash_pipes.size() < 4)
-		remove_child(pipes[0])
-		cash_pipes.append(pipes.pop_front())
-		if cash_pipes.size() > 4:
-			cash_pipes.pop_front().queue_free()
+		(pipes.pop_front() as Pipe).queue_free()
 	
 	_next_texture = randf() * _texture_pairs.size() as int
+
+func _pipes_clear() -> void:
+	for pipe in pipes: pipe.queue_free()
+	pipes.clear()
+	_index = 0
