64a5301c675e7427e8af73b8013caa0f4ac25dac
yukkie8058 <yukkie8058@gmail.com>
2025-07-05 16:46:28 +0900

    メニュー画面で、モバイル端末では別のテキストを表示する。 Resolve #4

--- ui/idle.gd
+++ ui/idle.gd
@@ -5,7 +5,10 @@ extends Control
 
 func _enter_tree() -> void:
 	_instruction.visible = false
+	
+	var mobile := ["mobile", "web_android", "web_ios"].any(OS.has_feature)
+	_instruction.text = "" if mobile else "PRESS SPACE\nOR\n"
+	_instruction.text += "%s ANYWHERE" % ("TOUCH" if mobile else "CLICK")
 
 func _instruction_timer_timeout() -> void:
 	_instruction.visible = not _instruction.visible
-
