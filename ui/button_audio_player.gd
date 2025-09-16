077275b8d71236727dca6c59041a4492e391b713
yukkie8058 <yukkie8058@gmail.com>
2025-09-16 16:58:57 +0900

    UIの各画面をシーンに分割し、ボタン押下時の音再生をリファクター

---
+++ ui/button_audio_player.gd
@@ -0,0 +1,27 @@
+@tool
+extends AudioStreamPlayer
+
+var _pressed_callables: Dictionary[BaseButton, Callable]
+
+func _enter_tree() -> void:
+	get_tree().node_added.connect(_tree_node_added)
+
+func _exit_tree() -> void:
+	get_tree().node_added.disconnect(_tree_node_added)
+
+func _tree_node_added(node: Node) -> void:
+	if node is BaseButton:
+		var button := node as BaseButton
+		button.theme_changed.connect(_button_theme_changed.bind(button))
+		_pressed_connect(button)
+
+func _button_theme_changed(button: BaseButton) -> void:
+	if _pressed_callables.has(button):
+		button.pressed.disconnect(_pressed_callables[button])
+	_pressed_connect(button)
+
+func _pressed_connect(button: BaseButton) -> void:
+	var playback := get_stream_playback() as AudioStreamPlaybackPolyphonic
+	var audio := (button.get_theme_icon("audio_pressed") as ThemeExValue).value as AudioStream
+	_pressed_callables[button] = playback.play_stream.bind(audio)
+	button.pressed.connect(_pressed_callables[button])
---
+++ utils/theme_ex_value.gd
@@ -0,0 +1,32 @@
+@tool
+class_name ThemeExValue
+extends Texture2D
+
+@export_custom(PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_ALWAYS_DUPLICATE)
+var value: Resource:
+	set = _set_value
+
+func _init() -> void:
+	if value != null:
+		value.changed.connect(emit_changed)
+
+func _set_value(new_value: Resource) -> void:
+	if value == new_value: return
+	
+	if value != null:
+		value.changed.disconnect(emit_changed)
+	if new_value != null:
+		new_value.changed.connect(emit_changed)
+	
+	value = new_value
+	
+	emit_changed()
+
+func _get_width() -> int: return 0
+func _get_height() -> int: return 0
+
+func _validate_property(property: Dictionary) -> void:
+	var script_variable := property["usage"] as int & PROPERTY_USAGE_SCRIPT_VARIABLE != 0
+	var resource := (property["name"] as String).begins_with("resource_")
+	if not (script_variable or resource):
+		property["usage"] = PROPERTY_USAGE_NONE
