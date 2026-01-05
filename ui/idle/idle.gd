76d7ede136476f99dd6c8f8ab8e5fefd140b437c
yukkie8058 <yukkie8058@gmail.com>
2026-01-06 01:14:29 +0900

    メインメニューに音の切り替えボタンを追加。 Resolve #16 (#19)

--- ui/idle/idle.gd
+++ ui/idle/idle.gd
@@ -1,6 +1,7 @@
 extends MarginContainer
 
 @export_group("Component")
+@export var _sound_toggle: Button
 @export var _version: LinkButton
 @export var _instruction: Label
 
@@ -13,6 +14,12 @@ func _enter_tree() -> void:
 	var mobile := ["mobile", "web_android", "web_ios"].any(OS.has_feature)
 	_instruction.text = "" if mobile else "PRESS SPACE\nOR\n"
 	_instruction.text += "%s ANYWHERE" % ("TOUCH" if mobile else "CLICK")
+
+func _sound_toggle_pressed() -> void:
+	var bus := 0
+	var mute := not AudioServer.is_bus_mute(bus)
+	AudioServer.set_bus_mute(bus, mute)
+	_sound_toggle.theme_type_variation = "SoundToggle%s" % ("Off" if mute else "On")
 
 func _instruction_timer_timeout() -> void:
 	_instruction.visible = not _instruction.visible
